0c8794757862e2c25001c22890f55a8baf441e28
Luiz Quadros <luizgustavoquadros00@gmail.com>
2026-03-23 11:23:15 -0300

    update

---
+++ src/exercicios/ex11.java
@@ -0,0 +1,11 @@
+package exercicios;
+
+public class ex11 {
+    public static void main(String[] args) {
+        int i = 10000;
+        while (i >= 1){
+            System.out.println(i);
+            i--;
+        }
+    }
+}
