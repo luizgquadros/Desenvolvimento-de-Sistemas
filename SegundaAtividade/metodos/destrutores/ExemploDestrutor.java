39ba58cd94dba9e5ad83f66c10e6271629738b94
Luiz Quadros <luizgustavoquadros00@gmail.com>
2026-04-13 15:15:33 -0300

    update

---
+++ SegundaAtividade/metodos/destrutores/ExemploDestrutor.java
@@ -0,0 +1,12 @@
+package metodos.destrutores;
+
+public class ExemploDestrutor {
+    protected void finalize() {
+        System.out.println("Objeto está sendo removido pelo Garbage Collector");
+    }
+    public static void main(String[] args) {
+        ExemploDestrutor exemplo = new ExemploDestrutor();
+        exemplo = null; // torna objeto disponível para remoção
+        System.gc(); // sugere execução do Garbage Collector
+    }
+}
