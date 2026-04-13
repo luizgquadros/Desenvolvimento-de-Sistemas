761ed225d37357ac03b445a42bafee980c3581fd
Luiz Quadros <luizgustavoquadros00@gmail.com>
2026-04-13 14:27:11 -0300

    update

---
+++ SegundaAtividade/metodos/retorno/DadosAluno.java
@@ -0,0 +1,11 @@
+package metodos.retorno;
+
+public class DadosAluno {
+    public String nomeAluno() {
+        return "Maria Silva";
+    }
+    public static void main(String[] args) {
+        DadosAluno aluno = new DadosAluno();
+        System.out.println("Nome do aluno: " + aluno.nomeAluno());
+    }
+}
