3f64d89e9d61c2faa61eeb0036669aad9ab5a1a0
Luiz Quadros <luizgustavoquadros00@gmail.com>
2026-05-04 11:13:23 -0300

    update: correções finais

--- SegundaAtividade/exercicios/metodos/retornoparametros/MetodosComRetornoEParametros1.java
+++ SegundaAtividade/exercicios/metodos/retornoparametros/MetodosComRetornoEParametros1.java
@@ -1,4 +1,12 @@
 package exercicios.metodos.retornoparametros;
 
 public class MetodosComRetornoEParametros1 {
+    public int multiplicar(int x, int y) {
+        return x * y;
+    }
+    public static void main(String[] args) {
+        MetodosComRetornoEParametros1 calc = new MetodosComRetornoEParametros1();
+        int resultado = calc.multiplicar(10, 10);
+        System.out.println("Resultado: " + resultado);
+    }
 }
